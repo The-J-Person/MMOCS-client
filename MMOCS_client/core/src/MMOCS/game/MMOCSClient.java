4c912c362d93380628500dfb50254ec80865628e
ofirx2 <ofirkubla@hotmail.com>
2015-04-05 22:45:43 +0300

    here is a project for the client that does almost nothing :)

---
+++ MMOCS_client/core/src/MMOCS/game/MMOCSClient.java
@@ -0,0 +1,37 @@
+package MMOCS.game;
+
+import java.net.Socket;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MMOCSClient extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Socket skt;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		try{
+			skt = new Socket("imfake", 3453);
+			img = new Texture("badlogic.jpg");
+			skt.close();
+		}
+		catch(Exception e){
+			img = new Texture("sadface.jpg");
+		}
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+}
